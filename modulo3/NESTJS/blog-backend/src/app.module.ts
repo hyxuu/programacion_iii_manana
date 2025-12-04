d51fa5dad70bb9118daadc3c6077de9ec5a3d885
hyxuu <hyxuu05@gmail.com>
2025-12-04 09:59:07 -0500

    Clase 2025-12-04

--- modulo3/NESTJS/blog-backend/src/app.module.ts
+++ modulo3/NESTJS/blog-backend/src/app.module.ts
@@ -14,6 +14,7 @@ import { BasicsModule } from './basics/basics.module';
 import { RestauranteModule } from './RB/restaurante/restaurante.module';
 import { ProductoModule } from './RB/producto/producto.module';
 import { CategoriaModule } from './RB/categoria/categoria.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
   imports: [
@@ -43,6 +44,7 @@ import { CategoriaModule } from './RB/categoria/categoria.module';
     RestauranteModule,
     ProductoModule,
     CategoriaModule,
+    MailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
