615a475a4c63fb5c37e5139b0c37054984ac14f0
hyxuu <hyxuu05@gmail.com>
2025-12-06 18:45:07 -0500

    fix Sendgrid

--- modulo3/NESTJS/blog-backend/src/mail/mail.controller.ts
+++ modulo3/NESTJS/blog-backend/src/mail/mail.controller.ts
@@ -18,5 +18,9 @@ export class MailController {
     const result = await this.mailService.sendMail(dto);
     return new SuccessResponseDto('Correo enviado con Gmail', result);
   }
-
+  @Post('sendgrid')
+  async sendSendGrid(@Body() dto: SendMailDto) {
+    const result = await this.mailService.sendWithSendGrid(dto);
+    return new SuccessResponseDto('Correo enviado con SendGrid', result);
+  }
 }
